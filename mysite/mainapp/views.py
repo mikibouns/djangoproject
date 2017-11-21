da240d11ba04166ff8e85308f1b05aa74536c684
mikibouns <mikibouns@gmail.com>
2017-11-21 13:18:26 +0200

    add date to nav

--- mysite/mainapp/views.py
+++ mysite/mainapp/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, render_to_response
 import json, os
+import datetime
+
+current_date = datetime.date.today()
 
 def main(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'current_date': current_date})
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    return render(request, 'contacts.html', {'current_date': current_date})
 
 def collections(request):
     with open(os.path.join(os.getcwd(), 'static/json/base.json'), encoding='utf-8') as file:
         collections = json.load(file)
-    return render(request, 'collections.html', {'collections': collections})
+    return render(request, 'collections.html', {'collections': collections, 'current_date': current_date})
 
