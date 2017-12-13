208019e5381a8dbcd1da7e7173eb5f60e1ccc774
mikibouns <mikibouns@gmail.com>
2017-12-13 12:32:54 +0200

    redesigned the purchase, fixed the js script _2

--- mysite/static/js/slider.js
+++ mysite/static/js/slider.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.dot').click(function() {
+        $('.dot:first').addClass('active');
         var value_pk = $(this).attr('value');
         value_pk = '/basket/add/' + value_pk + '/';
         var path_img = '/media/' + this.id + '.jpg'
